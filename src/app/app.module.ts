4413cc67b9460307247f3e7cd38595a205b0ba3b
maretluce <maretluce@gmail.com>
2021-02-02 08:38:01 -0500

    route configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { routing, appRoutingProviders } from './app.routing';
 
 /* components */
 import { VotingboxComponent } from './components/votingbox/votingbox.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CharactersComponent } from './components/characters/characters.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlankpageComponent } from './components/blankpage/blankpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VotingboxComponent,
     HeaderComponent,
-    CharactersComponent
+    CharactersComponent,
+    HomeComponent,
+    BlankpageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 
---
+++ src/app/app.routing.ts
@@ -0,0 +1,15 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { BlankpageComponent } from './components/blankpage/blankpage.component';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'new-page', component: BlankpageComponent },
+  { path: '**', component: HomeComponent }
+];
+
+export const appRoutingProviders: any[] = [];
+//export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);
